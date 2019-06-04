6167ae315b1185432f20fd493298b892acb072ba
michaelstsnow <michael.st.snow@gmail.com>
2019-06-04 18:55:04 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -36,11 +36,8 @@ class Student
     sql=<<-SQL
       INSERT INTO students (name,grade) VALUES (?,?)
     SQL
-
     DB[:conn].execute(sql,self.name,self.grade)
-
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
-
   end
 
 
