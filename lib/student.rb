4d108ba43b72b72b0b7b241e6d263bde16d2b42e
michaelstsnow <michael.st.snow@gmail.com>
2019-06-04 18:50:57 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -25,6 +25,15 @@ class Student
   end
 
   def self.drop_table
+    sql=<<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade TEXT
+      )
+    SQL
+
+    DB[:conn].execute(sql)
   end
 
 
